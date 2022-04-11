54036d204e5b5808b4bde404c7eaf60b4caa82fb
David Bui <dav_edu@tutamail.com>
2022-04-11 21:48:07 +1000

    cart icon & dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,9 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as Logo } from '../../assets/dd.svg'
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -25,7 +29,9 @@ const Navigation = () => {
                 <span className="nav-link" onClick={signOutUser}> SIGN OUT</span>)
                 : ( <Link className="nav-link" to='/auth'> SIGN IN </Link> )
             }
+            <CartIcon />
           </div>
+          <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
