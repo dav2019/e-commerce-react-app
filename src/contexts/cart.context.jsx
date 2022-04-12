5a331cfff4a3a4df056fe28996190a75c8ad9e04
David Bui <dav_edu@tutamail.com>
2022-04-12 17:49:34 +1000

    cart total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -44,17 +44,24 @@ export const CartContext = createContext({
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
     cartItemCount: 0,
+    cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartItemCount, setCartItemCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
         const count = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         setCartItemCount(count);
-    }, [cartItems])
+    }, [cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+        setCartTotal(newCartTotal);
+    }, [cartItems]);
 
     const addItemToCart = (product) => {
         setCartItems(addCartItem(cartItems, product));
@@ -68,7 +75,7 @@ export const CartProvider = ({ children }) => {
         setCartItems(clearCartItem(cartItems, cartItemToClear));
     }
 
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, removeItemFromCart, clearItemFromCart, cartItemCount };
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, removeItemFromCart, clearItemFromCart, cartItemCount, cartTotal };
     
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
     
