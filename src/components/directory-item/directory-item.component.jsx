658791bce6818a5b51fbbff7e2ddb5d31a8d5bf2
David Bui <dav_edu@tutamail.com>
2022-04-13 18:28:37 +1000

    styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,21 @@
-import './directory-item.styles.scss'
+import './directory-item.styles.jsx'
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx';
+import { Link } from 'react-router-dom';
+
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
-    return (
-        <div className="directory-item-container">
-           <div className="background-image" style={{
-             backgroundImage: `url(${imageUrl})`
-             }} />
-         <div className="body">
-           <h2>{title}</h2>
-           {/* <p>Shop Now</p> */}
-         </div>
-       </div>
-    )
-}
+  const { imageUrl, title } = category;
+  const url = `/shop/${title.toLowerCase()}`
+  return (
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        {/* <h2>{title}</h2> */}
+        {/* <p>Shop Now</p> */}
+        <Link to={url}>{title}</Link>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
 
 export default DirectoryItem;
