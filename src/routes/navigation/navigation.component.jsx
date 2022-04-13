2adc3b166569a08b39b5d327d1566e35bcd91a08
David Bui <dav_edu@tutamail.com>
2022-04-13 17:13:57 +1000

    styled components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Logo } from '../../assets/dd.svg'
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import './navigation.styles.scss'
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles'
 
 
 const Navigation = () => {
@@ -20,23 +20,23 @@ const Navigation = () => {
     
     return (
       <Fragment>
-        <div className="navigation">
-          <Link className="logo-container" to='/'>
+        <NavigationContainer>
+          <LogoContainer to='/'>
             {/* <Logo className="logo" /> */}
             <h2>CUSTOM PC</h2>
-          </Link> 
-          <div className="nav-links-container">
-            <Link className="nav-link" to='/shop'>
+          </LogoContainer> 
+          <NavLinks>
+            <NavLink to='/shop'>
                 SHOP
-            </Link>
+            </NavLink>
             {currentUser ? (
-                <span className="nav-link" onClick={signOutUser}> SIGN OUT</span>)
-                : ( <Link className="nav-link" to='/auth'> SIGN IN </Link> )
+                <NavLink as='span' onClick={signOutUser}> SIGN OUT</NavLink>)
+                : ( <NavLink to='/auth'> SIGN IN </NavLink> )
             }
             <CartIcon />
-          </div>
+          </NavLinks>
           {isCartOpen && <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet />
       </Fragment>
     );
