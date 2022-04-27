a4b7c119e015c43a554e378fd3ea9cb3f8113880
David Bui <dav_edu@tutamail.com>
2022-04-27 16:05:21 +1000

    Redux-Persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 // import { UserProvider } from './contexts/user.context';
 // import { CategoriesProvider } from './contexts/categories.context';
 // import { CartProvider } from './contexts/cart.context';
 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -18,11 +19,11 @@ import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-         
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
            <App />
-          
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
